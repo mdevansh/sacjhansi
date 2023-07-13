65a2df28fb39511333fcabe2a5a78e54e5e59bc6
Devansh Mishra <mdevansh28@gmail.com>
2023-07-13 20:09:17 +0530

    Add dance .gif

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ function App() {
           Edit <code>src/App.js</code> and save to reload. Lolz!!.
           <br></br>
           Used Github workflows for this :) . YAyyyy!YY!!
+          <br></br>
+          <br></br>
         </p>
+        <IMG SRC="https://media4.giphy.com/media/4DUjG2ei31nA2evo5g/200w.gif?cid=790b7611hq60sh2uu6n5hrt22c47uge4rw7n1b5ym4exe2cf&ep=v1_gifs_search&rid=200w.gif&ct=g"></IMG>
+
         <a
           className="App-link"
           href="https://reactjs.org"
